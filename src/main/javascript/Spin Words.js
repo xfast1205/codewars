7be8ea605cf3cc1ce358f810e093afc7327d1c7b
Андрей <andrey@MacBook-Pro-Andrej.local>
2018-07-05 18:45:47 +0300

    Spin Big Words (js)

---
+++ src/main/javascript/Spin Words.js
@@ -0,0 +1,16 @@
+function spinWords(str){
+    var arrayOfWords = str.split(' ');
+
+    var reversedString = '';
+    for (let i = 0; i < arrayOfWords.length; i++) {
+        if (arrayOfWords[i].length > 4) {
+            reversedString += arrayOfWords[i].split('').reverse().join('');
+        } else {
+            reversedString += arrayOfWords[i];
+        }
+        if (i != arrayOfWords.length - 1) {
+            reversedString += ' ';
+        }
+    }
+    return reversedString;
+}
