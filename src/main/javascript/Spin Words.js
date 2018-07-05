ca9d5393e250365f13dc2cad61e4d9979cc6fa91
Андрей <andrey@MacBook-Pro-Andrej.local>
2018-07-05 18:52:10 +0300

    small refactoring for Spin words

--- src/main/javascript/Spin Words.js
+++ src/main/javascript/Spin Words.js
@@ -3,14 +3,12 @@ function spinWords(str){
 
     var reversedString = '';
     for (let i = 0; i < arrayOfWords.length; i++) {
-        if (arrayOfWords[i].length > 4) {
+        if (arrayOfWords[i].length > 4)
             reversedString += arrayOfWords[i].split('').reverse().join('');
-        } else {
+        else
             reversedString += arrayOfWords[i];
-        }
-        if (i != arrayOfWords.length - 1) {
+        if (i != arrayOfWords.length - 1)
             reversedString += ' ';
-        }
     }
     return reversedString;
 }
